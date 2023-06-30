82e0e891dd70b4d8e2ee54b6f522544a94d6cbbd
SilvVF <david.silver232@gmail.com>
2023-06-30 01:55:48 -0400

    add room database and module for dependency injection

--- app/src/main/java/io/silv/workout/MainActivity.kt
+++ app/src/main/java/io/silv/workout/MainActivity.kt
@@ -3,7 +3,6 @@ package io.silv.workout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
